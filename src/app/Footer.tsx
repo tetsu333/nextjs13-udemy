b4610ca272a0542bfeb726ecb7eb7e707a88e04a
tetsu <cda07c5117@gmail.com>
2023-10-04 21:38:33 +0900

    学習終了

---
+++ src/app/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className='py-2 px-4 border-t'>
+         <small>@2023 tetsucode</small>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
+
+
---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image'
+import ArticleList from './components/ArticleList'
+import { getAllArticles } from '@/blogAPI'
+import { supabase } from '@/utils/supabaseClient'
+
+export default async function Home() {
+  // const articles = await getAllArticles()
+  // console.log(supabase)
+
+  const API_URL = process.env.NEXT_PUBLIC_API_URL
+  const res = await fetch(`${API_URL}/api`, {cache: "no-store"})
+  const articles = await res.json()
+
+  return (
+    <div className="md:flex">
+    <section className="w-full md:w-2/3 flex flex-col items-center px-3">
+      <ArticleList articles={articles} />
+      <div className="flex items-center py-8">
+        <a
+          href="#"
+          className="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center"
+        >
+          1
+        </a>
+        <a
+          href="#"
+          className="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center"
+        >
+          2
+        </a>
+        <a
+          href="#"
+          className="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3"
+        >
+          Next <i className="fas fa-arrow-right ml-2"></i>
+        </a>
+      </div>
+    </section>
+
+    <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
+      <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
+        <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          varius enim in eros elementum tristique.
+        </p>
+      </div>
+      <div className="bg-white shadow-md rounded p-4 mb-6 mt-4 w-full">
+        <h3 className="font-bold text-gray-900 mb-2">Category</h3>
+        <ul className="text-gray-600 mt-2">
+          <li>
+            <a href="#">Technology</a>
+          </li>
+          <li>
+            <a href="#">Automotive</a>
+          </li>
+          <li>
+            <a href="#">Finance</a>
+          </li>
+          <li>
+            <a href="#">Sports</a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  </div>
+  )
+}
